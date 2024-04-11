b75211388639c99399f64ce7cb53ffd3a5eb179f
joel0115 <joel0115@gmail.com>
2024-04-11 15:39:24 +0800

    function declaration

--- Arbitrage.py
+++ Arbitrage.py
@@ -1,3 +1,5 @@
+import deepcopy from copy
+
 liquidity = {
     ("tokenA", "tokenB"): (17, 10),
     ("tokenA", "tokenC"): (11, 7),
@@ -10,3 +12,30 @@ liquidity = {
     ("tokenC", "tokenE"): (10, 8),
     ("tokenD", "tokenE"): (60, 25),
 }
+
+Bmax = -1
+pathMax = ""
+
+def getAmountOut(amountIn, reserveIn, reserveOut):
+    amountOut = 997*amountIn*reserveOut / (1000*reserveIn + 997*amountIn)
+    return amountOut
+
+def dfs(liq, maxPathLen, curToken, curVal, endToken, path):
+    if(curToken == endToken and len(path) != 1):
+        if(Bmax < curVal):
+            Bmax = curVal
+            pathMax = path
+            return
+        if(len(path) > maxPathLen):
+            return
+
+
+
+
+
+if __name__ == '__main__':
+    liq = deepcopy(liquidity)
+    
+    dfs(liq, 10, 'B', 5, 'B', "B")
+
+    print()
